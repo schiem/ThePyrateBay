2b01603c5e4f5fb83604db66054cd2c8b5f7e7a9
Schiem <schiem01@gmail.com>
2014-07-20 23:56:36 -0400

    Cleaned things up a bit more.  It will now iterate through the list of torrents and try each one until it runs out.  Also removed two extraneous functions and worked on some documentation.  Next up is to add scheduling so that it will run at intervals, updating the set of dates as it does so.

--- Pyrate.py
+++ Pyrate.py
@@ -5,9 +5,14 @@
 #  v 0.1
 #  Michael Yoder
 #
-#  Reqs:   
-#         ThePirateBay
-#         LibTorrent
+#  External Reqs:  BeautifulSoup (4) 
+#                  ThePirateBay
+#                  LibTorrent
+#  Standard Reqs:  urllib2
+#                  time
+#                  datetime
+#                  configparser
+#                  os
 ################################################
 
 import urllib2
@@ -23,6 +28,9 @@ import configparser
 import os
 
 SETTINGS_PATH = "settings.ini"
+
+
+############### IMDB Functions ######################
 
 '''
 A function to search imdb. 
@@ -41,11 +49,10 @@ def search(date_range, rating):
     language = 'languages=en'
     title_type = 'title_type=feature'
     sort = 'sort=user_rating,{0}'.format(rating)
-    votes = 'num_votes=100,'
+    votes = 'num_votes=50,'
     #the url to query imdb
     search_url = '{0}/{1}&{2}&{3}&{4}&{5}&{6}'.format(url, title, language, votes, release, sort, title_type)
     results = urllib2.urlopen(search_url)
-    print "Searching using url : {0}".format(search_url)
     return results.read()
 
 
@@ -61,9 +68,10 @@ def get_titles_and_ratings(imdb_html):
     links = {}
     for title in bs.find_all('td', {'class' : 'title'}):
          links[title.find('a').text] = float(title.find('span', {'class' : 'value'}).text)
-    print links
     return links
 
+
+############### ThePirateBay Functions ######################
 '''
 Searches ThePirateBay for a given title in the category MOVIES.
 Parameter: title
@@ -72,7 +80,7 @@ Return:
     The search result with the most seeders.  If no results are
     found or if there aren't enough seeders, return None.
 '''
-def get_torrent(title):
+def search_torrent(title):
     result = None
     pirate = TPB('https://thepiratebay.org')
     search = pirate.search(title, category=CATEGORIES.VIDEO.MOVIES).order(ORDERS.SEEDERS.DES)
@@ -84,70 +92,7 @@ def get_torrent(title):
     else:
         return result
 
-'''
-Use libtorrent to download the data from a torrent file.
-Parameter: torrent
-    The file path of the torrent to download.
-Return:
-    None.
-'''
-def download_torrent(torrent):
-    
-    #create a new libtorrent session
-    ses = lt.session()
-    ses.listen_on(6881, 6891)
-
-    #open the torrent file
-    e = lt.bdecode(open(torrent, 'rb').read())
-    info = lt.torrent_info(e)
-
-    params = { 'save_path': SAVE_PATH, \
-        'storage_mode': lt.storage_mode_t.storage_mode_sparse, \
-        'ti': info }
-
-    #add the torrent to the session
-    h = ses.add_torrent(params)
-
-    #if we're not seeding yet...
-    while (not h.is_seed()):
-        s = h.status()
-        print('%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
-                (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
-                s.num_peers, s.state))
-
-        time.sleep(1)
-
-'''
-Get a torrent file from ThePirateBay.
-Parameter: torrent_url
-    The path to the torrent file.
-Return:
-    The file name that the torrent was saved under.
-'''
-def download_torrent_file(torrent_url):
-    file_name = torrent_url.split('/')[-1]
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print(status)
-
-    f.close()
-    return file_name
-
+############### Torrenting Functions ######################
 '''
 Convert a magnet file to a torrent and download the torrent associated with it.
 Parameter: magnet
@@ -158,10 +103,10 @@ Return:
 def download_magnet(magnet):
     ses = lt.session()
 
-
-    params = { 'save_path': SAVE_PATH, \
+    params = { 'save_path': str(get_setting("save")), \
         'duplicate_is_error' : True }
     #add the magnet
+    
     h = lt.add_magnet_uri(ses, magnet, params)
 
     #get the metadata from the magnet
@@ -176,6 +121,34 @@ def download_magnet(magnet):
                 (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
                 s.num_peers, s.state))
         time.sleep(1)
+
+'''
+Attempt to get a find a certain number of torrents from The Pirate Bay from a list
+of titles. The function will continue to search until it has either found the supplied
+number of torrents or reached the end of the list.
+Parameter: movies
+    The list of movie titles to search for.
+Parameter: number
+    The number of torrents to find.
+Return:
+    A list of torrent objects.
+'''
+def get_torrents(movies, number):
+    torrents = []
+    i = 0
+    while i<len(movies) and len(torrents) < number:
+        print("Attempting to get torrent for {0}".format(movies[i]))
+        torrent = search_torrent(movies[i])
+        if(torrent):
+            print("Found a torrent!")
+            torrents.append(torrent)
+        else:
+            print("No torrent found...")
+        i += 1
+    return torrents
+
+
+############### Settings Functions ######################
 
 '''
 If settings.conf doesn't exist, create it and fill it with user input.
@@ -199,19 +172,47 @@ def load_settings():
     settings = settings["Search"]
     return settings
 
+'''
+Gets a setting from the settings.ini file.
+Parameter: setting
+    A string of the setting to get.
+Return:
+    The value of the setting in the file. Will raise
+    a KeyError if no setting is found.
+'''
 def get_setting(setting):
     return load_settings()[setting]
 
+'''
+Subtracts a specified number of years from a date given in the format
+YYYY-MM-DD,YYYY-MM-DD
+Parameter: date
+    A string representing the date.
+Parameter: years
+    The number of years to subtract.
+Return:
+    A string representing the new date in the format YYYY-MM-DD,YYYY-MM-DD
+'''
 def subtract_years(date, years):
-    dates = date.split(" ")
+    dates = date.split(",")
     new_dates = []
     for d in dates:
         new_dates.append(str(int(d[0:4]) - years) + d[4:])
-    return " ".join(new_dates)
+    return ",".join(new_dates)
 
-def keys_from_dict(_dict, number, order):
+'''
+Moves the keys of a dictionary to a list based on the numerical value
+in the dictionary.  Assumes that the values in the dictionary are ints.
+Parameter: _dict
+    The dictionary to iterate through
+Parameter: order
+    Whether to sort by highest or lowerst value.
+Return:
+    An ordered list containing the keys of the dictionary.
+'''
+def sort_keys_by_value(_dict, order):
     keys = []
-    for i in range(number):
+    for i in range(len(_dict)):
         if(order == 'asc'):
             _key = max(_dict.iterkeys(), key=(lambda key: _dict[key]))
         elif(order  == 'desc'):
@@ -223,57 +224,81 @@ def keys_from_dict(_dict, number, order):
         keys.append(_key)
     return keys
 
+
+
+
+############### Main Functions ##########################
+
+'''
+Generate a list of all the best and worst movies in a given range of years.
+Return:
+    Two lists of the best and worst movies ordered by rating.
+'''
 def get_movies():
     date = get_setting("dates")
     if(date == 'None'):
         #if no date is selected, then the date used should be today
         date = str(datetime.date.today())
-        date = "{0} {1}".format(date, date)
+        date = "{0},{1}".format(date, date)
 
+    #get the number of years to search
     years = int(get_setting("years"))
+    
+    #set up empty variables
     good_html = []
     bad_html = []
     
     good_results = {}
     bad_results = {}
     
+    #get the search results for the specified date for the number of 
+    #years specified
     for i in range(years): 
-        print date, i
+        #get the new date to search for
         search_date = subtract_years(date, i+1)
+        
+        #get the raw html from imdb
         good_html = search(search_date, 'asc')
         bad_html = search(search_date, 'desc')
-
         
+        #translate the raw html to titles and ratings
         good_results.update(get_titles_and_ratings(good_html))
         bad_results.update(get_titles_and_ratings(bad_html))
-    to_download = keys_from_dict(good_results, int(get_setting("good")), 'asc')
-    to_download += keys_from_dict(bad_results, int(get_setting("bad")), 'desc')
-    return to_download 
     
+    #turn the titles and ratings into a sorted list of titles
+    good_movies = sort_keys_by_value(good_results, 'asc')
+    bad_movies = sort_keys_by_value(bad_results, 'desc')
+    return good_movies, bad_movies
+    
+'''
+The main function.  Searches IMDB for movies in the specified date
+range, then attempts to download them from ThePirateBay.
+'''
+def run():
+    #get a list of movies we want
+    good_movies, bad_movies = get_movies()
+    torrents = []
+
+    #Try to find torrents for those movies
+    print("Getting good movies...")
+    to_download = get_torrents(good_movies, int(get_setting("good")))
+    print("Getting bad movies...")
+    to_download += get_torrents(bad_movies, int(get_setting("bad")))
+    
+    #remove duplicates
+    to_download = list(set(to_download))
+    
+    #if no torrents were found, stop
+    if(len(to_download) == 0):
+        print("Sorry, none of the movies were found on the piratebay...exiting quietly.")
+
+    #otherwise, download the torrents (ensuring that we don't download duplicates)
+    else:
+        for torrent in to_download:
+            download_magnet(torrent.magnet_link)
+
 
 if __name__ == "__main__":
     configure()
     if(get_setting("time") == "None"):
-        #get a list of movies we want
-        movie_list = get_movies()
-        torrents = []
-
-        #Try to find torrents for those movies
-        for movie in movie_list:
-            print "Attempting to get torrent for {0}".format(movie)
-            torrent = get_torrent(movie)
-            if(torrent):
-                print("Success!")
-                torrents.append(torrent)
-            else:
-                print("Sorry, no torrent found.")
-        
-        #if no torrents were found, stop
-        if(len(torrents) == 0):
-            print("Sorry, none of the movies were found on the piratebay...exiting quietly.")
-        
-        #otherwise, download the torrents
-        else:
-            for torrent in torrents:
-                download_magnet(torrent.magnet_link)
-
+        run()
